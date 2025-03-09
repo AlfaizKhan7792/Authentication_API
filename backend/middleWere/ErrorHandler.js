6ae4c31cbf33dfff7e3ae7838e821912c6a31a44
AlfaizKhan7792 <alfaizkhan7792@gmail.com>
2025-03-09 17:17:38 +0530

    production

--- backend/middleWere/ErrorHandler.js
+++ backend/middleWere/ErrorHandler.js
@@ -5,7 +5,7 @@ const MiddleWere = (err , req , res , next) =>{
 
     res.json({
         message : err.message,
-        stack : process.env.NODE_ENV === "development" ? err.stack : null
+        stack : process.env.NODE_ENV === "production" ? null :  err.stack 
     })
 }
 
